7cfb4f07c97c75e495ba42dc671e6402f2a76f58
Daniel <42664403+danieljimenez0255@users.noreply.github.com>
2022-03-25 20:06:35 -0700

    project config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "lh3.googleusercontent.com",
+      "gateway.ipfscdn.io",
+      "storage.opensea.io",
+      "via.placeholder.com",
+      "cdn.sanity.io",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
