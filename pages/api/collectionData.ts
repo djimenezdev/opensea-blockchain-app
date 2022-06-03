63ffce519d510224987da49f20d6f12a7ee16d7b
Daniel Jimenez <daniel@djimenezdev.com>
2022-06-02 22:22:37 -0700

    cors config for api

--- pages/api/collectionData.ts
+++ pages/api/collectionData.ts
@@ -1,11 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { client } from "../../lib/sanity";
+import { client } from '../../lib/sanity';
+import NextCors from 'nextjs-cors';
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-  ) {
-      if(req.method=== "POST" && req.body.url === "/collections"){
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await NextCors(req, res, {
+    // Options
+    methods: ['POST', 'GET'],
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    origin: ['http://localhost:3000', 'https://os-market-clone.vercel.app/'],
+  });
+  if (req.method === 'POST' && req.body.url === '/collections') {
     const query = `*[_type=="marketItems" && contractAddress == "${req.body.collectionId}"] {
         "imageUrl":profileImage.asset->url,
         "bannerImageUrl": bannerImage.asset->url,
@@ -17,15 +24,34 @@ export default async function handler(
         floorPrice,
         "allOwners":owners[]->,
         description
-      }`
-      const collectionData = await client.fetch(query);
-      res.json(collectionData)
-    } else {
-await client.create({_id:req.body.address, _type:'users', userName:req.body.address,walletAddress:req.body.address}).then(async ({walletAddress, userName}:{walletAddress:string, userName:string}) => {
-    await client.patch(process.env.MARKET_ID).insert('after','owners[-1]',[{_key:walletAddress,userName,walletAddress}]).commit();
-    res.end()
-}).catch((err:any) => res.status(409).send('Document already exists'))
-
-}
-
+      }`;
+    const collectionData = await client.fetch(query);
+    res.json(collectionData);
+  } else {
+    await client
+      .create({
+        _id: req.body.address,
+        _type: 'users',
+        userName: req.body.address,
+        walletAddress: req.body.address,
+      })
+      .then(
+        async ({
+          walletAddress,
+          userName,
+        }: {
+          walletAddress: string;
+          userName: string;
+        }) => {
+          await client
+            .patch(process.env.MARKET_ID)
+            .insert('after', 'owners[-1]', [
+              { _key: walletAddress, userName, walletAddress },
+            ])
+            .commit();
+          res.end();
+        }
+      )
+      .catch((err: any) => res.status(409).send('Document already exists'));
+  }
 }
--- pages/collections/[collectionId].tsx
+++ pages/collections/[collectionId].tsx
@@ -1,13 +1,18 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import Header from '../../components/Header';
 import { CgWebsite } from 'react-icons/cg';
 import { AiOutlineInstagram, AiOutlineTwitter } from 'react-icons/ai';
+import { FaEthereum } from 'react-icons/fa';
 import { HiDotsVertical } from 'react-icons/hi';
-import NFTCard from "../../components/Collection/NFTCard";
-import { NextPage } from "next";
-import {useAddress, useMarketplace, useNFTCollection} from "@thirdweb-dev/react";
-import Image from "next/image";
+import NFTCard from '../../components/Collection/NFTCard';
+import { NextPage } from 'next';
+import {
+  useAddress,
+  useMarketplace,
+  useNFTCollection,
+} from '@thirdweb-dev/react';
+import Image from 'next/image';
 
 const style = {
   bannerImageContainer: `relative z-0 h-[20vh] w-screen overflow-hidden flex justify-center items-center`,
@@ -15,7 +20,7 @@ const style = {
   infoContainer: `relative w-screen px-4`,
   midRow: `w-full flex justify-center text-white`,
   endRow: `w-full flex justify-end text-white`,
-  profileContainer:`relative w-40 h-40 mt-[-4rem]  rounded-full border-2 border-[#202225]`,
+  profileContainer: `relative w-40 h-40 mt-[-4rem]  rounded-full border-2 border-[#202225]`,
   profileImg: ` object-cover rounded-full`,
   socialIconsContainer: `flex text-3xl mb-[-2rem] mr-2`,
   socialIconsWrapper: `w-44`,
@@ -30,37 +35,37 @@ const style = {
   ethLogo: `relative h-6 w-10 r-2`,
   statName: `text-lg w-full text-center mt-1`,
   description: `text-[#8a939b] text-xl w-max-1/4 flex-wrap mt-4`,
-}
+};
 
 interface collectionStructure {
-  imageUrl:string
-  bannerImageUrl:string
-  volumeTraded:number|undefined
-  createdBy:string
-  contractAddress:string
-  creator:string
-  title:string
-  floorPrice:string
-  allOwners:Array<any>
-  description:string
- 
-
-
+  imageUrl: string;
+  bannerImageUrl: string;
+  volumeTraded: number | undefined;
+  createdBy: string;
+  contractAddress: string;
+  creator: string;
+  title: string;
+  floorPrice: string;
+  allOwners: Array<any>;
+  description: string;
 }
 
-const Collection:NextPage = () => {
+const Collection: NextPage = () => {
   const router = useRouter();
-  const collectionId = router?.query?.collectionId?router?.query?.collectionId as string:undefined ;
+  const collectionId = router?.query?.collectionId
+    ? (router?.query?.collectionId as string)
+    : undefined;
   const nftCollection = useNFTCollection(collectionId);
-  const marketplace = useMarketplace('0xF1402ECdDeC1a83de2a49dd0e0eF25925Ad47892')
-  const [collection,setCollection] = useState<collectionStructure>();
-  const [nfts, setNfts] = useState<any>([])
+  const marketplace = useMarketplace(
+    '0xF1402ECdDeC1a83de2a49dd0e0eF25925Ad47892'
+  );
+  const [collection, setCollection] = useState<collectionStructure>();
+  const [nfts, setNfts] = useState<any>([]);
   const [listings, setListings] = useState<any>([]);
   const address = useAddress();
-  
-  
+
   useEffect(() => {
-    if(!nftCollection) return;
+    if (!nftCollection) return;
     (async () => {
       const nfts = await nftCollection.getAll();
       setNfts(nfts);
@@ -68,43 +73,48 @@ const Collection:NextPage = () => {
   }, [nftCollection]);
 
   useEffect(() => {
-    if(!marketplace) return;
+    if (!marketplace) return;
     (async () => {
       setListings(await marketplace.getAllListings());
     })();
-  },[marketplace]);
+  }, [marketplace]);
 
-
- /*  useEffect(() => {
+  /*  useEffect(() => {
   setOwners([...new Set<string>(nfts.map(({owner}:{owner:string}) => owner))])
   },[nfts]) */
 
   useEffect(() => {
-    if(!collectionId) return;
+    if (!collectionId) return;
     const fetchCollectionData = async () => {
-        const fetchCollection = await fetch('https://os-market-clone.vercel.app/api/collectionData',{
-          method:'POST',
-          body:JSON.stringify({collectionId: collectionId, url:"/collections"}),
-          headers:{
-            'Content-Type':'application/json'
-          }
-        }).then((res) => res.json());
- setCollection(fetchCollection[0])
-}
-    fetchCollectionData()
-  }, [ collectionId])
+      const fetchCollection = await fetch(
+        'https://os-market-clone.vercel.app/api/collectionData',
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            collectionId: collectionId,
+            url: '/collections',
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      ).then((res) => res.json());
+      setCollection(fetchCollection[0]);
+    };
+    fetchCollectionData();
+  }, [collectionId]);
 
   return (
-    <div className="overflow-hidden">
+    <div className='overflow-hidden'>
       <Header />
       <div className={style.bannerImageContainer}>
-        <Image className={style.bannerImage} src={collection?.bannerImageUrl ? collection?.bannerImageUrl: 'https://via.placeholder.com/200'} alt="banner" layout="fill" priority/>
+        <FaEthereum />
       </div>
       <div className={style.infoContainer}>
         <div className={style.midRow}>
           <div className={style.profileContainer}>
-          <Image className={style.profileImg} src={collection?.imageUrl ? collection?.imageUrl : 'https://via.placeholder.com/200'} alt="profile pic" layout="fill" />
-        </div>
+            <FaEthereum />
+          </div>
         </div>
         <div className={style.endRow}>
           <div className={style.socialIconsContainer}>
@@ -113,13 +123,16 @@ const Collection:NextPage = () => {
                 <div className={style.socialIcon}>
                   <CgWebsite />
                 </div>
-                <div className={style.divider} /><div className={style.socialIcon}>
+                <div className={style.divider} />
+                <div className={style.socialIcon}>
                   <AiOutlineInstagram />
                 </div>
-                <div className={style.divider} /><div className={style.socialIcon}>
+                <div className={style.divider} />
+                <div className={style.socialIcon}>
                   <AiOutlineTwitter />
                 </div>
-                <div className={style.divider} /><div className={style.socialIcon}>
+                <div className={style.divider} />
+                <div className={style.socialIcon}>
                   <HiDotsVertical />
                 </div>
               </div>
@@ -131,54 +144,58 @@ const Collection:NextPage = () => {
         </div>
         <div className={style.midRow}>
           <div className={style.createdBy}>
-            Created By 
-            <span className="text-[#2081e2]"> {collection?.creator}</span>
+            Created By
+            <span className='text-[#2081e2]'> {collection?.creator}</span>
           </div>
         </div>
         <div className={style.midRow}>
           <div className={style.statsContainer}>
             <div className={style.collectionStat}>
-              <div className={style.statValue}>
-                {nfts?.length}
-              </div>
+              <div className={style.statValue}>{nfts?.length}</div>
               <div className={style.statName}>items</div>
             </div>
             <div className={style.collectionStat}>
-              <div className={style.statValue}>{collection?.allOwners ? collection.allOwners.length : ''}</div>
+              <div className={style.statValue}>
+                {collection?.allOwners ? collection.allOwners.length : ''}
+              </div>
               <div className={style.statName}>owners</div>
             </div>
             <div className={style.collectionStat}>
               <div className={style.statValue}>
                 <div className={style.ethLogo}>
-                <Image src="https://storage.opensea.io/files/6f8e2979d428180222796ff4a33ab929.svg" alt="eth logo" layout="fill"/>
-               </div>
+                  <FaEthereum />
+                </div>
                 {collection?.floorPrice}
               </div>
               <div className={style.statName}>floor price</div>
-            </div><div className={style.collectionStat}>
+            </div>
+            <div className={style.collectionStat}>
               <div className={style.statValue}>
-              <div className={style.ethLogo}>
-                <Image src="https://storage.opensea.io/files/6f8e2979d428180222796ff4a33ab929.svg" alt="eth logo" layout="fill"/>
-              </div>
-                {!!collection?.volumeTraded && collection?.volumeTraded/1000}.5K
+                <div className={style.ethLogo}>
+                  <FaEthereum />
+                </div>
+                {!!collection?.volumeTraded && collection?.volumeTraded / 1000}
+                .5K
               </div>
               <div className={style.statName}>ETH</div>
             </div>
-           
           </div>
         </div>
         <div className={style.midRow}>
-  <div className={style.description}>{collection?.description}</ div>
-</div>
+          <div className={style.description}>{collection?.description}</div>
+        </div>
       </div>
-      <div className="flex flex-wrap justify-center">
-        {
-          nfts.map((item:any, id:string) => (
-            <NFTCard key={id} nftItem={item} title={collection?.title} listings={listings}/>
-          ))
-        }
+      <div className='flex flex-wrap justify-center'>
+        {nfts.map((item: any, id: string) => (
+          <NFTCard
+            key={id}
+            nftItem={item}
+            title={collection?.title}
+            listings={listings}
+          />
+        ))}
       </div>
     </div>
-  )
-}
-export default Collection
+  );
+};
+export default Collection;
